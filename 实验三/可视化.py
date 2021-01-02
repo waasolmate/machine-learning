522db649dc1f468255f8e33a92057da86d418713
waasolmate <52588588+waasolmate@users.noreply.github.com>
2021-01-02 15:20:20 +0800

    Update 可视化.py

--- 实验三/可视化.py
+++ 实验三/可视化.py
@@ -11,18 +11,15 @@ pd.set_option('display.max_colwidth', 10000)
 pd.set_option('display.max_columns',None)
 pd.set_option('display.max_rows',None)
 
-# csvFile = open(r"C:\Users\16920\Desktop\2_5.csv",'w',newline='',encoding='utf-8')
-# writer = csv.writer(csvFile,dialect='excel')
-#
-#
-# f = open(r"C:\Users\16920\Desktop\2_5.txt",'r',encoding='GB2312')
-# for line in f.readlines():
-#     line=line.replace(',','\t')
-#     csvRow = line.split()
-#     writer.writerow(csvRow)
-#
-# f.close()
-# csvFile.close()
+csvFile = open(r"C:\Users\16920\Desktop\2_5.csv",'w',newline='',encoding='utf-8')
+writer = csv.writer(csvFile,dialect='excel')
+f = open(r"C:\Users\16920\Desktop\2_5.txt",'r',encoding='GB2312')
+for line in f.readlines():
+     line=line.replace(',','\t')
+     csvRow = line.split()
+     writer.writerow(csvRow)
+f.close()
+csvFile.close()
 
 df=pd.read_csv(r'D:\学习资料\机器学习\实验三 K-Means\k=3.csv',encoding='gbk',header=None)
 
