caaa674d7926dc9eff785c4ca5b7fb8a918be3ab
waasolmate <52588588+waasolmate@users.noreply.github.com>
2021-01-02 15:23:15 +0800

    Update 可视化.py

--- 实验三/可视化.py
+++ 实验三/可视化.py
@@ -2,29 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import csv
 import numpy as np
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.rcParams['axes.unicode_minus']=False
-np.set_printoptions(threshold=np.inf)
-
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', 10000)
-pd.set_option('display.max_columns',None)
-pd.set_option('display.max_rows',None)
-
-csvFile = open(r"C:\Users\16920\Desktop\2_5.csv",'w',newline='',encoding='utf-8')
-writer = csv.writer(csvFile,dialect='excel')
-f = open(r"C:\Users\16920\Desktop\2_5.txt",'r',encoding='GB2312')
-for line in f.readlines():
-     line=line.replace(',','\t')
-     csvRow = line.split()
-     writer.writerow(csvRow)
-f.close()
-csvFile.close()
-
+#读取文件数据
 df=pd.read_csv(r'D:\学习资料\机器学习\实验三 K-Means\k=3.csv',encoding='gbk',header=None)
 
+#聚为几类d_type即为类数
 d_type=3
 
+#生成散点图
 fig=plt.figure(0)
 color=['red','blue','green','orange','purple']
 for i in range(d_type):
